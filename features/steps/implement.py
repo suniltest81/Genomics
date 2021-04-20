553c4d8090d5e25ab92b3939109d8a7785527f91
suniltest81 <60527071+suniltest81@users.noreply.github.com>
2021-04-20 01:19:43 +0100

    Couple more tests added

--- features/steps/implement.py
+++ features/steps/implement.py
@@ -15,7 +15,7 @@ def step_impl(context, header_conent_type):
 @given(u'I set sample REST API url')
 def step_impl(context):
     global api_url
-    api_url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=6a7482c902e1caa93bc41ecebeca463d'
+    api_url = 'http://api.openweathermap.org/data/2.5/weather'
 
 
 @then(u'I receive invalid HTTP response code 204')
@@ -31,15 +31,14 @@ def step_impl(context, request_name):
 # END POST Scenario
 
 # START GET Scenario
-@given(u'I perform the GET request on products end point')
-#@given(u'I perform the GET request on products end point with id "{id}"')
-def step_impl(context):
-    api_endpoints['GET_URL'] = api_url
-    #print('url :'+api_endpoints['GET_URL'])
+@given(u'I perform the GET request on products end point with city "{city_name}"')
+def step_impl(context, city_name):
+    api_endpoints['GET_URL'] = api_url+'?q='+city_name+'&appid=6a7482c902e1caa93bc41ecebeca463d'
+    print('url :'+api_endpoints['GET_URL'])
 
 @given(u'I Set GET posts api endpoint for products')
 def step_impl(context):
-    api_endpoints['GET_URL'] = api_url+'/v1/products'
+    api_endpoints['GET_URL'] = api_url
     print('url :'+api_endpoints['GET_URL'])
 
 @when(u'Send GET HTTP request')
@@ -64,51 +63,13 @@ def step_impl(context, request_name):
     print(response_texts)
     assert response_texts[request_name] is not None
 
-# END GET Scenario
-
-#START DELETE
-@given(u'I Set DELETE posts api endpoint for "{id}"')
-def step_impl(context,id):
-    api_endpoints['DELETE_URL'] = api_url + '/v1/product/'+id
-    print('url :' + api_endpoints['DELETE_URL'])
-
-@when(u'I Send DELETE HTTP request')
-def step_impl(context):
-    # sending get request and saving response as response object
-    response = requests.delete(url=api_endpoints['DELETE_URL'])
-    # extracting response text
-    response_texts['DELETE'] = response.text
-    print("DELETE response :" + response.text)
-    # extracting response status_code
-    statuscode = response.status_code
-    response_codes['DELETE'] = statuscode
-
-@then(u'I receive valid HTTP response code 204 for "{request_name}"')
+@then(u'I receive an invalid HTTP response code 404 for "{request_name}"')
 def step_impl(context, request_name):
-    print('Get rep code for '+request_name+':'+ str(response_codes[request_name]))
-    assert response_codes[request_name] is 204
-#END DELETE
+    print('Get rep code updated for '+request_name+':'+ str(response_codes[request_name]))
+    print(response_codes[request_name])
+    assert response_codes[request_name] == 404
 
-#START PUT
-@given(u'I Set PUT posts api endpoint for "{id}"')
-def step_impl(context,id):
-    api_endpoints['PUT_URL'] = api_url + '/v1/product/'+id
-    print('url :' + api_endpoints['PUT_URL'])
 
-@when(u'I Set Update request Body')
-def step_impl(context):
-    request_bodies['PUT']={"name": "Leather boots", "price": 350}
-
-@when(u'Send PUT HTTP request')
-def step_impl(context):
-    response = requests.put(url=api_endpoints['PUT_URL'], json=request_bodies['PUT'], headers=request_headers)
-    # extracting response text
-    response_texts['PUT'] = response.text
-    print("update response :" + response.text)
-    # extracting response status_code
-    statuscode = response.status_code
-    response_codes['PUT'] = statuscode
-#END PUT
 
 
 
--- features/steps/test.feature
+++ features/steps/test.feature
@@ -1,16 +1,27 @@
-Feature: Test QA App controller in Sample REST API testing framework
+Feature: Testing weather api in Sample REST API testing framework
 
 Background:
 	Given I set sample REST API url
 
-Scenario:  Performing the GET request on products end point with id
-    #Given I perform the GET request on products end point with city "London"
-	Given I perform the GET request on products end point
+Scenario:Performing a valid GET request on weather api by city name
+    Given I perform the GET request on products end point with city "London"
     When I Set HEADER param request content type as "application/json"
 	And Send GET HTTP request
     Then I receive valid HTTP response code 200 for "GET"
 	And Response BODY "GET" is non-empty
+	
+Scenario:Performing a valid GET request on weather api by city name
+    Given I perform the GET request on products end point with city "Paris"
+    When I Set HEADER param request content type as "application/json"
+	And Send GET HTTP request
+    Then I receive valid HTTP response code 200 for "GET"
+	And Response BODY "GET" is non-empty	
 
+Scenario:  Performing an invalid GET request on weather api with a random name
+    Given I perform the GET request on products end point with city "test"
+    When I Set HEADER param request content type as "application/json"
+	And Send GET HTTP request
+    Then I receive an invalid HTTP response code 404 for "GET"	
 
 
 
